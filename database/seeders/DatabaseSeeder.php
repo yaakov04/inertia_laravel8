767212719cd3b7e2897047e109614f599c3a3ecd
Yaakov <dbrionesmtz@yahoo.com>
2021-11-22 12:10:37 -0600

    configure los modelos, las migraciones, los factories y el controllador

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Note;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'email'=>'d@admin.com',
+            'password'=> bcrypt('123456789')
+        ]);
+
+        Note::factory(30)->create();
     }
 }
