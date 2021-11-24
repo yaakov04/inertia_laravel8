a3b42f2840e0429b7c566942f0a0cb56b58778fb
Yaakov <dbrionesmtz@yahoo.com>
2021-11-24 11:38:40 -0600

    configuracion inicial de rutas y archivos

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,15 +16,20 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+});*/
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
+Route::view('/', 'index');
+
+Route::get('dashboard', [PageController::class, 'dashboard'] )
+->middleware('auth:sanctum')
+->name('dashboard');
+
+Route::resource('notes', NoteController::class)
+    ->middleware('auth:sanctum');
